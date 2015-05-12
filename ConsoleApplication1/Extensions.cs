e46f8bd15e43d4cdd68fd7cd4521935793f422f0
knarf7112 <knarf7112@gmail.com>
2015-05-12 13:58:50 +0800

    #0001:Create Test Project

---
+++ ConsoleApplication1/Extensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelimiterExtention
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// 依定界符號來切割列舉
+        /// </summary>
+        /// <typeparam name="T">型別</typeparam>
+        /// <param name="source">來源列舉物件</param>
+        /// <param name="delimiter">定界符號</param>
+        /// <returns>依定界符號分離後後可列舉的列舉(陣列)物件</returns>
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, IEnumerable<T> delimiter)
+        {
+            //定界符號轉列表物件
+            var delimiterList = delimiter.ToList();
+            //暫存列表
+            var outputBuffer = new List<T>();
+            //counter
+            var m = 0;
+            //列舉所有項目
+            foreach (var item in source)
+            {
+                //若此項目是定界符號
+                if (item.Equals(delimiterList[m]))
+                {
+                    m++;
+
+                    //若(記數器+1)等於定界符號的數量=>即定界符號數量等同於記數器次數
+                    if (m == delimiterList.Count)
+                    {
+                        //記數器歸零
+                        m = 0;
+
+                        //若暫存列表有資料
+                        if (outputBuffer.Count > 0)
+                        {
+                            //傳出去前一次切割後的暫存列表
+                            yield return outputBuffer;
+                            //暫存列表歸零
+                            outputBuffer = new List<T>();
+                        }
+                    }
+                }
+                else
+                {
+                    //這段不太懂會何要加 (暫存列表加入定界列表列舉)
+                    outputBuffer.AddRange(delimiterList.Take(m));
+
+                    //若項目為定界符號[0]
+                    if (item.Equals(delimiterList[0]))
+                    {
+                        //記數器推進1
+                        m = 1;
+                    }
+                    else
+                    {
+                        //記數器歸零
+                        m = 0;
+                        //要資料項目加入此block的暫存列表
+                        outputBuffer.Add(item);
+                    }
+                }
+            }
+
+            outputBuffer.AddRange(delimiterList.Take(m));
+            //傳出最後一段暫存列表(因迴圈已跑完,所以補上最後一段被切割的暫存列表)
+            if (outputBuffer.Count > 0)
+            {
+                yield return outputBuffer;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.IO;
+using System.IO.Compression;
+
+namespace DelimiterExtention
+{
+    class c1 { }
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            c1 c = null;
+            bool bo = c is c1;
+            byte[] input = new byte[]{ 1, 2, 3,99, 4, 6,7,8,9,99,10,11,12 };
+            var ie = input.Skip(3).Take(4);
+            //input
+            byte[] delimiter = new byte[] { 99 };
+            //切割input array 定界符號 => 99
+            IEnumerable<IEnumerable<byte>> qqq = input.Split(delimiter);
+            foreach (var i in qqq)
+            {
+                var item = i;
+            }
+            string[] strAry = Encoding.ASCII.GetString(input).Split('c').Select(x => x).ToArray();
+            //var splitBytes = FriendAccess.Extensions.Split<byte[]>();
+        }
+
+        /// <summary>
+        /// GZip壓縮和解壓縮測試
+        /// https://msdn.microsoft.com/zh-tw/library/system.io.compression.gzipstream.write.aspx
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            UnicodeEncoding uniEncode = new UnicodeEncoding();
+
+            byte[] bytesToCompress = uniEncode.GetBytes("我example text to compress and decompress");
+            //--------------------------
+            byte[] byteEncoding1 = Encoding.Unicode.GetBytes("我example text to compress and decompress");
+            byte[] byteEncoding2 = Encoding.ASCII.GetBytes("我example text to compress and decompress");
+            byte[] byteEncoding3 = Encoding.BigEndianUnicode.GetBytes("我example text to compress and decompress");
+            byte[] byteEncoding4 = Encoding.UTF8.GetBytes("我example text to compress and decompress");
+            byte[] byteEncoding5 = Encoding.GetEncoding(950).GetBytes("我example text to compress and decompress");
+            //----------------------------
+            Console.WriteLine("starting with: " + uniEncode.GetString(bytesToCompress));
+
+            using (FileStream fileToCompress = File.Create("examplefile.gz"))
+            {
+                using (GZipStream compressionStream = new GZipStream(fileToCompress, CompressionMode.Compress))
+                {
+                    compressionStream.Write(bytesToCompress, 0, bytesToCompress.Length);
+                }
+            }
+
+            byte[] decompressedBytes = new byte[bytesToCompress.Length];
+            using (FileStream fileToDecompress = File.Open("examplefile.gz", FileMode.Open))
+            {
+                using (GZipStream decompressionStream = new GZipStream(fileToDecompress, CompressionMode.Decompress))
+                {
+                    decompressionStream.Read(decompressedBytes, 0, bytesToCompress.Length);
+                }
+            }
+
+            Console.WriteLine("ending with: " + uniEncode.GetString(decompressedBytes));
+        }
+
+    }
+}
